5b24804755dcdffdef425abba9a096d032bb6d21
Sharjeel <92874026+ShaYri12@users.noreply.github.com>
2024-07-15 03:56:05 +0500

    Update Corporate.tsx

--- src/app/components/Corporate.tsx
+++ src/app/components/Corporate.tsx
@@ -4,35 +4,34 @@ const Corporate = () => {
   return (
     <div className="bg-[#F26A45]">
       <div className="overflow-x-hidden">
-        <h2 className="text-white pt-[160px] lg:ps-[120px] lg:pe-0 px-[81px] text-[#F26A45] text-[64px] font-[700] leading-[86.24px] tracking-[1%] font-clashdisplay-regular">
+        <h2 className="text-white md:pt-[160px] pt-[100px] lg:ps-[120px] lg:pe-0 md:px-[81px] sm:px-[25px] px-[20px] text-[#F26A45] md:text-[64px] text-[43px] font-[700] md:leading-[86.24px] leading-[50px] tracking-[1%] font-clashdisplay-regular">
           CORPORATE
         </h2>
-        <div className="flex lg:flex-row flex-col  items-end justify-between mb-auto h-full text-[30px] text-[#EFE8E9] font-[400] leading-[40.42px] tracking-[9%] font-clashdisplay-regular">
-          <div className="relative flex me-auto h-[1268px] w-full lg:w-1/2 lg:order-1 order-2">
-            <div className="absolute z-2] top-[14%] lg:left-[30%] left-2 w-[820px]">
+        <div className="flex lg:flex-row flex-col  items-end justify-between mb-auto h-full md:text-[30px] text-[24px] text-[#EFE8E9] font-[400] leading-[40.42px] tracking-[9%] font-clashdisplay-regular">
+          <div className="relative flex me-auto md:h-[1268px] w-full lg:w-1/2 lg:order-1 order-2">
+            <div className="absolute z-2] md:top-[14%] sm:top-[14%] top-[13%] lg:left-[30%] left-2 w-[820px]">
               <span className="relative z-[1]">
                 <img
-                  className="h-fit w-[1052px]"
+                  className="h-fit lg:w-[1052px] md:w-[750px] sm:w-[620px] w-[300px]"
                   src="/assets/CORPORATE.svg"
                   alt="modern"
                 />
               </span>
               <span className="relative z-[2]">
                 <img
-                  className="w-[700px] mt-[-15px]"
+                  className="lg:w-[700px] md:w-[650px] sm:w-[540px] w-[260px] md:mt-[-15px] sm:mt-[-10px] mt-[-5px]"
                   src="/assets/PROJECTS.svg"
                   alt="architecture"
                 />
               </span>
             </div>
             <img
-              className="object-cover h-full w-full z-[1]"
-              height={"100%"}
+              className="object-cover md:h-full sm:h-[50%] h-[10%] w-full z-[1]"
               src="/assets/corporate.png"
               alt="corporate"
             />
           </div>
-          <div className="flex flex-col gap-[60px] lg:px-[30px] px-[81px] pt-[120px] lg:pb-[100px] pb-[90px] w-full lg:w-[60%] lg:order-2 order-1">
+          <div className="flex flex-col gap-[60px] lg:px-[30px] md:px-[81px] sm:px-[25px] px-[20px] lg:pt-[120px] md:pt-[80px] pt-[40px] lg:pb-[100px] pb-0 w-full lg:w-[60%] lg:order-2 order-1">
             <p>
               Echipa noastra de experti in constructii si amenajari interioare
               ofera servicii complete pentru companiile care îsi doresc sa
