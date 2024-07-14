06ba51f4eac9f1dcdea01d98d79ee3144bd50687
Sharjeel <92874026+ShaYri12@users.noreply.github.com>
2024-07-15 04:53:25 +0500

    Update OurVision.tsx

--- src/app/components/OurVision.tsx
+++ src/app/components/OurVision.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const OurVision = () => {
   return (
     <div className="overflow-x-hidden">
-      <h2 className="text-white pt-[160px] lg:ps-[120px] px-[81px] text-[#F26A45] text-[64px] font-[700] leading-[86.24px] tracking-[1%] font-clashdisplay-regular">
+      <h2 className="text-white pt-[160px] lg:px-[120px] md:px-[80px] sm:px-[30px] text-[#F26A45] md:text-[64px] text-[43px] font-[700] md:leading-[86.24px] leading-[50px] tracking-[1%] font-clashdisplay-regular">
         Viziunea noastra
       </h2>
-      <div className="flex lg:flex-row flex-col lg:order-1 order-2 items-end justify-between mb-auto h-full text-[#EFE8E9] text-[30px] font-[400] leading-[40.42px] tracking-[9%] font-clashdisplay-regular">
-        <div className="flex flex-col gap-[60px] ps-[81px] py-[200px] w-full lg:w-[60%]">
+      <div className="flex lg:flex-row flex-col lg:order-1 order-2 items-end justify-between mb-auto h-full text-[#EFE8E9] md:text-[30px] text-[22px] font-[400] leading-[40.42px] tracking-[9%] font-clashdisplay-regular">
+        <div className="flex flex-col gap-[60px] lg:ps-[81px] md:ps-[80px] ps-[25px] pe-[25px]  lg:pe-0 md:pe-[80px] md:py-[200px] py-[50px] w-full lg:w-[60%]">
           <p>
             Viziunea noastra asupra arhitecturii moderne se bazeaza pe eleganta
             si simplitate. Credem ca designul bine gandit îmbunatateste atat
@@ -21,15 +21,37 @@ const OurVision = () => {
             executie perfecta a lucrarii.
           </p>
         </div>
-        <div className="relative flex me-auto h-[1268px] w-full lg:w-1/2 lg:order-2 order-1">
-          <div className="absolute z-[999] top-[14%] right-0 w-[820px]">
+
+        <div className="relative me-auto h-[1268px] w-full lg:w-1/2 lg:order-2 order-1 vision-large">
+          <div className="absolute z-[999] lg:top-[14%] lg:right-0 w-[820px]">
             <img
-              className="h-fit w-[464px] ms-[px]"
+              className="h-fit lg:w-[464px] w-[400px] ms-[5px]"
               src="/assets/MODERN.svg"
               alt="modern"
             />
             <img
-              className="w-[1276px] mt-[-25px]"
+              className="lg:w-[1276px] md:w-[768px] mt-[-25px]"
+              src="/assets/architectureText.svg"
+              alt="architecture"
+            />
+          </div>
+          <img
+            className="object-cover h-full w-full"
+            height={"100%"}
+            src="/assets/architecture.svg"
+            alt="architecture"
+          />
+        </div>
+
+        <div className="relative me-auto md:h-[1268px] h-[600px] w-full lg:w-1/2 lg:order-2 order-1 vision-small">
+          <div className="absolute z-[999] lg:top-[14%] top-[35%] lg:right-0 lg:w-[820px] md:w-[740px] sm:w-[600px] w-[300px]  mx-auto left-1/2 transform -translate-x-1/2">
+            <img
+              className="h-fit lg:w-[464px] md:w-[400px] sm:w-[332px] w-[200px] md:ms-[16px] sm:ms-[10px] ms-[5px]"
+              src="/assets/MODERN.svg"
+              alt="modern"
+            />
+            <img
+              className="lg:w-[1276px] md:w-[758px] sm:mt-[-11px] mt-[-7px]"
               src="/assets/architectureText.svg"
               alt="architecture"
             />
