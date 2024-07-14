6e40c948d6d39e2ab7d8830b6ecc104c804c3713
Sharjeel <92874026+ShaYri12@users.noreply.github.com>
2024-07-15 03:23:37 +0500

    Update CustomerOpinion.tsx

--- src/app/components/CustomerOpinion.tsx
+++ src/app/components/CustomerOpinion.tsx
@@ -1,17 +1,32 @@
+"use client";
 import React from "react";
 
 const CustomerOpinion = () => {
+  const handleGoToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Optional: smooth scrolling animation
+    });
+  };
+
   return (
-    <div className="bg-white flex flex-col gap-[80px] py-[100px] lg:px-[157] md:px-[100px] px-[30px] font-clashdisplay-regular text-[#F26A45] text-[64px] tracking-[9%] leading-[86.24px]">
-      <h2 className="font-[700]">OPINIA CLIENTILOR</h2>
-      <span className="flex flex-col font-[400] text-black mx-auto max-w-[780px] items-center justify-center">
-        <h3 className="text-[64px] leading-[86.24px] tracking-[9%]">
+    <div className="bg-white flex flex-col md:gap-[80px] md:gap-[40px] gap-[10px] md:py-[100px] py-[80px] lg:px-[157px] md:px-[70px] px-[25px] font-clashdisplay-regular">
+      <h2 className="font-[700] text-[#F26A45] md:text-[64px] text-[30px] tracking-[9%] md:leading-[86.24px] leading-[60px]">
+        OPINIA CLIENTILOR
+      </h2>
+      <span className="flex flex-col font-[400] text-black max-w-[780px] md:items-center items-start md:justify-centexr justify-start">
+        <h3 className="md:text-[64px] text-[25px] md:leading-[86.24px] leading-[32px] tracking-[9%]">
           “Printre putinele firme serioase ramase pe piata”
         </h3>
-        <p className="flex items-end justify-end ms-auto italic text-[30px] leading-[40.42px] tracking-[9%]">
+        <p className="flex items-end justify-end md:ms-auto ms-0 md:me-0 me-auto italic md:text-[30px] text-[18px] md:leading-[40.42px] leading-[27px] tracking-[9%]">
           Anton Andrei
         </p>
       </span>
+      <div className="flex w-full justify-end ms-auto">
+        <button onClick={handleGoToTop}>
+          <img src="/assets/up-arrow.svg" alt="Go to top" width="68.38px" />
+        </button>
+      </div>
     </div>
   );
 };
