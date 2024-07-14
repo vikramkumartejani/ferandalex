a4ac1e8e1b0831493368ec2c706b798a2711d4dd
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-07-15 01:55:18 +0500

    ABout & Service Page Added

---
+++ src/app/about/page.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="text-white font-clashdisplay-regular">
+      <section className="relative">
+        <img
+          src="/assets/section-shape.png"
+          alt=""
+          className="absolute top-0 left-0"
+        />
+        <img src="/assets/about-section-bg.jpeg" alt="" />
+        <h1 className="font-extrabold absolute top-[60%] lg:top-2/3 ml-[5%] uppercase text-[20px] leading-[25px] xl:text-[100px] x;:leading-[96px]">
+          DESPRE NOI: <br /> de la planuri la realitate
+        </h1>
+      </section>
+
+      <section className="grid  md:grid-cols-2 lg:pt-[200px] items-center justify-center gap-[80px]  px-[5%] lg:px-0 lg:pl-[5%] max-h-[850px] py-[5%]">
+        <div>
+          <h3 className="text-[40px] lg:text-[100px] leading-[123px]">
+            Misiunea noastra
+          </h3>
+          <p className="text-[20px] lg:text-[40px] leading-[30px] md:leading-[50px]">
+            Suntem o echipa de profesionisti pasionati, dedicati sa construim
+            viitorul. Cu o experienta bogata, ne angajam sa depasim asteptarile
+            si sa transformam visele in realitate.
+          </p>
+        </div>
+
+        <div className="hidden lg:block px-0 h-full w-full lg:rounded-l-[77px] overflow-hidden">
+          <img
+            src="/assets/about-section-bg1.jpeg"
+            className="h-full w-full object-contain"
+          />
+        </div>
+
+        <div className="lg:hidden w-[90%] h-full rounded-[50px] mx-auto overflow-hidden">
+          <img
+            src="/assets/about-section-bg1.jpeg"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </section>
+
+      <section className="space-y-[40px] lg:pt-[300px] md:space-y-[60px] lg:space-y-[150px] px-[10%]">
+        <h2 className="text-[40px] lg:text-[100px] leading-[123px] text-center">
+          Cine suntem
+        </h2>
+        <p className="text-[20px] lg:text-[40px] leading-[30px] md:leading-[50px]">
+          Ferandalex, o companie fondată în 2018 de un grup de profesioniști
+          pasionați, a pornit cu o viziune clară: construirea unui viitor mai
+          bun. <br /> <br /> De la început, Ferandalex s-a concentrat pe
+          calitatea excepțională a lucrărilor și pe abordarea transparentă a
+          proiectelor. Cu o echipă de arhitecți, ingineri și tehnicieni
+          altamente calificați, compania a reușit să construiască case,
+          apartamente, clădiri de birouri și spații comerciale care au depășit
+          așteptările clienților.
+          <br />
+          <br />
+          La Ferandalex, suntem mai mult decât o companie; suntem o familie - o
+          familie unită de pasiune, profesionalism și dorința de a face o
+          diferență.
+        </p>
+
+        <div className="relative w-full overflow-hidden">
+          <img
+            src="/assets/about-section-bg2.jpeg"
+            alt="Background"
+            className="w-full h-auto"
+            style={{ height: "auto" }}
+          />
+          <h1 className="text-[60px] lg:text-[220px] xl:text-[280px] text-start 3xl:text-[320px] leading-none lg:leading-[220px] font-extrabold absolute top-[60%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">
+            WE ARE A <span className="text-[#CCE716]">FAMILY</span>
+          </h1>
+        </div>
+
+        <h2 className="text-[40px] lg:text-[100px] leading-[50px] lg:leading-[123px] text-center">
+          Angajamentul nostru
+        </h2>
+      </section>
+
+      <section>
+        <div className="relative w-full h-[500px] lg:h-[900px] xl:h-[1000px] 2xl:h-[1100px]  overflow-hidden">
+          <img
+            src="/assets/about-section-bg3.jpeg"
+            alt="About Section Background"
+            className="absolute top-0 left-0 w-full h-full object-cover "
+          />
+        </div>
+
+        <div>
+          <p className="text-[20px] px-[10%] py-[15%] lg:text-[40px] leading-[30px] md:leading-[50px]">
+            La Ferandalex, ne angajăm să oferim mai mult decât simple
+            construcții. Garantăm excelență în fiecare aspect al muncii noastre,
+            de la proiectare până la finalizare. <br /> <br /> Calitatea
+            excepțională este asigurată prin utilizarea materialelor premium și
+            a tehnologiilor de ultimă generație, în timp ce inovația continuă ne
+            permite să găsim soluții eficiente și durabile pentru fiecare
+            proiect.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/service/page.tsx
@@ -0,0 +1,228 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const NextArrow: React.FC<{ onClick?: () => void }> = ({ onClick }) => {
+  return (
+    <div
+      className="absolute top-1/2 transform text-[40px] font-bold -translate-y-1/2 -right-52 cursor-pointer"
+      onClick={onClick}
+    >
+      Next
+    </div>
+  );
+};
+
+const PrevArrow: React.FC<{ onClick?: () => void }> = ({ onClick }) => {
+  return (
+    <div
+      className="absolute top-1/2 transform -translate-y-1/2 text-[40px] font-bold -left-52 z-30 rounded-full p-2 cursor-pointer"
+      onClick={onClick}
+    >
+      Previous
+    </div>
+  );
+};
+
+const page = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
+  return (
+    <div className="text-white font-clashdisplay-regular">
+      <section className="relative">
+        <img
+          src="/assets/section-shape.png"
+          alt=""
+          className="absolute top-0 left-0"
+        />
+        <img src="/assets/about-section-bg.jpeg" alt="" />
+        <h1 className="font-extrabold absolute top-[60%] lg:top-2/3 ml-[5%] uppercase text-[20px] leading-[25px] xl:text-[100px] x;:leading-[96px]">
+          SERVICII
+        </h1>
+      </section>
+
+      <section className="px-[5%] lg:px-[10%] py-[10%] space-y-[15%] mx-[5%] lg:mx-[15%] border-l border-r border-white ">
+        <div className="relative">
+          <div className="relative max-h-[900px] rounded-3xl lg:rounded-[140px] overflow-hidden">
+            <img
+              src="/assets/service-section-bg.jpeg"
+              alt="Service Background"
+              className="object-cover w-full h-full"
+            />
+            <div className="absolute bottom-0 w-full bg-white/20 rounded-t-3xl lg:rounded-t-[140px] h-[50%] flex items-center justify-center text-[40px] lg:text-[120px] text-center backdrop-blur-sm">
+              Constructii
+            </div>
+          </div>
+        </div>
+        <p className="px-[10%] lg:px-[20%] text-[15px] lg:text-[30px] leading-[20px] lg:leading-[37px] ">
+          Ferandalex ofera servicii complete de constructii, de la planificare
+          si proiectare la executie si finisare. Echipa noastra de specialisti
+          cu vasta experienta se ocupa de toate etapele procesului de
+          constructie, adaptandu-se nevoilor specifice ale fiecarui client.{" "}
+          <br /> <br />
+          Fie ca doriti sa construiti o casa noua, sa renovati o cladire veche
+          sau sa implementati proiecte de amenajare exterioara, Ferandalex este
+          partenerul de incredere de care aveti nevoie.
+        </p>
+      </section>
+
+      <section className="border-t border-white ">
+        <div className="mx-[5%] lg:mx-[15%] border-l border-r  border-white ">
+          <div className="relative">
+            <div className="relative hidden lg:block  overflow-hidden">
+              <img
+                src="/assets/service-section-bg2.jpeg"
+                alt="Service Background"
+                className="object-cover w-full h-full"
+              />
+              <div className="absolute top-[15%] leading-[100px] font-extrabold right-0 h-[50%] text-[120px] text-start ">
+                INTERIOR <br /> <span className="gradient-text">DESIGN</span>
+              </div>
+            </div>
+
+            <div className="relative  lg:hidden h-[500px]  overflow-hidden">
+              <img
+                src="/assets/service-section-bg2.jpeg"
+                alt="Service Background"
+                className="object-cover w-full h-full"
+              />
+              <div className="absolute top-[15%] leading-[50px] font-extrabold right-0 h-[50%] text-[60px] text-start ">
+                INTERIOR <br /> <span className="gradient-text">DESIGN</span>
+              </div>
+            </div>
+          </div>
+          <p className="px-[10%] lg:px-[20%] text-[15px] lg:text-[30px] leading-[20px] lg:leading-[37px] py-[12%]">
+            Cu o abordare personalizata si o atentie meticuloasa la detalii,
+            echipa noastra va va ajuta sa va descoperiti stilul, nevoile si
+            aspiratiile pentru a le transpune in designul casei dumneavoastra.
+            Avem capacitatea de a vedea potentialul ascuns in fiecare spatiu si
+            de a-l transforma intr-un paradis personalizat, cu o functionalitate
+            impecabila. <br /> <br /> Lasati-ne pe noi sa va ghidam in aceasta
+            calatorie, sa va transformam visul intr-o realitate palpabila,
+            vibranta si plina de inspiratie. De la proiectarea si alegerea
+            materialelor, la gestionarea proiectelor si supravegherea
+            lucrarilor, vom fi alaturi de dumneavoastra la fiecare pas. La
+            Ferandalex, credem cu pasiune ca acasa ar trebui sa fie o reflectare
+            a identitatii dumneavoastra, un loc unde va puteti exprima liber si
+            va puteti simti cu adevarat bine.
+          </p>
+        </div>
+      </section>
+
+      <section className="border-t border-white ">
+        <div className="mx-[5%] lg:mx-[15%] border-l border-r  border-white ">
+          <div className="relative">
+            <div className="relative hidden lg:block overflow-hidden">
+              <img
+                src="/assets/service-section-bg3.jpeg"
+                alt="Service Background"
+                className="object-cover w-full h-full"
+              />
+              <div className="absolute top-[15%] leading-[100px] font-extrabold right-0 h-[50%] text-[120px] text-start ">
+                CLEANING
+              </div>
+              <div className="absolute bottom-0 w-full bg-black/30 py-[15%] px-[5%] flex items-center justify-center text-[30px] leading-[37px] text-start backdrop-blur-sm">
+                Pasiunea noastra pentru curatenie se traduce in spatii
+                impecabile si confortabile pentru dumneavoastra. Echipa noastra
+                de profesionisti se dedica utilizarii celor mai performante
+                produse si echipamente pentru a oferi o gama completa de
+                servicii de curatenie, adaptate nevoilor individuale. <br />{" "}
+                <br /> Fie ca doriti o curatenie generala saptamanala, o
+                pregatire minutioasa pentru mutare sau o intretinere atenta a
+                suprafetelor delicate, suntem aici sa va oferim solutii
+                personalizate. Flexibilitatea programului nostru si angajamentul
+                pentru calitate ne fac partenerul ideal pentru a mentine un
+                mediu curat si sanatos in locuintele si birourile dumneavoastra.
+                Contactati-ne astazi pentru a experimenta diferenta Ferandalex!
+              </div>
+            </div>
+
+            <div className="relative lg:hidden h-[1000px]  overflow-hidden">
+              <img
+                src="/assets/service-section-bg3.jpeg"
+                alt="Service Background"
+                className="object-cover w-full h-full"
+              />
+              <div className="absolute top-[15%] leading-[60px] font-extrabold right-0 h-[50%] text-[55px] text-start ">
+                CLEANING
+              </div>
+              <div className="absolute bottom-0 w-full bg-black/30 py-[15%] px-[5%] flex  items-center justify-center text-[15px] leading-[20px] text-start backdrop-blur-sm">
+                Pasiunea noastra pentru curatenie se traduce in spatii
+                impecabile si confortabile pentru dumneavoastra. Echipa noastra
+                de profesionisti se dedica utilizarii celor mai performante
+                produse si echipamente pentru a oferi o gama completa de
+                servicii de curatenie, adaptate nevoilor individuale. <br />{" "}
+                <br /> Fie ca doriti o curatenie generala saptamanala, o
+                pregatire minutioasa pentru mutare sau o intretinere atenta a
+                suprafetelor delicate, suntem aici sa va oferim solutii
+                personalizate. Flexibilitatea programului nostru si angajamentul
+                pentru calitate ne fac partenerul ideal pentru a mentine un
+                mediu curat si sanatos in locuintele si birourile dumneavoastra.
+                Contactati-ne astazi pentru a experimenta diferenta Ferandalex!
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="border-t border-white max-h-[1400px] overflow-hidden">
+        <div className="mx-[5%] lg:mx-[15%] border-l border-r  border-white ">
+          <h1 className="gradient-text text-[70px] lg:text-[180px] leading-[180px] font-extrabold flex justify-end">
+            GALLERY
+          </h1>
+
+          {/* Slick slider */}
+          <div className="flex flex-col justify-center w-full mx-auto ">
+            <Slider {...settings}>
+              <div>
+                <Image
+                  src="/assets/service-section-carousel-img.jpeg"
+                  alt="Slide 1"
+                  width={1000}
+                  height={600}
+                  className="  w-full h-auto"
+                />
+              </div>
+
+              <div>
+                <Image
+                  src="/assets/service-section-carousel-img.jpeg"
+                  alt="Slide 1"
+                  width={1000}
+                  height={600}
+                  className="  w-full h-auto"
+                />
+              </div>
+
+              <div>
+                <Image
+                  src="/assets/service-section-carousel-img.jpeg"
+                  alt="Slide 1"
+                  width={1000}
+                  height={600}
+                  className="  w-full h-auto"
+                />
+              </div>
+            </Slider>
+          </div>
+          {/* End  */}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default page;
