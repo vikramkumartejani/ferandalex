e815c746c5ad49410850557bc595425335fce133
Sharjeel <92874026+ShaYri12@users.noreply.github.com>
2024-07-15 05:58:07 +0500

    Update page.tsx

--- src/app/contact/thankyou/page.tsx
+++ src/app/contact/thankyou/page.tsx
@@ -9,20 +9,20 @@ const ThankYou = () => {
           <img
             height={"100%"}
             width={"100%"}
-            className="object-cover p-[30px] h-[1172px] w-full"
+            className="object-cover md:p-[30px] sm:p-[20px] p-[10px] h-[1172px] w-full"
             src="/assets/contact-img2.svg"
             alt="contact-img"
           />
-          <div className="w-full absolute top-1/2 flex flex-col  items-center justify-center gap-[30px] px-[30px]">
-            <h1 className="max-w-[800px] text-center justify-center text-white text-[121px] font-[400] leading-[119.31px]">
+          <div className="w-full absolute top-1/2 flex flex-col  items-center justify-center sm:gap-[30px] gap-[18px] px-[30px]">
+            <h1 className="max-w-[800px] text-center justify-center text-white md:text-[121px] sm:text-[89px] text-[40px] font-[400] md:leading-[119.31px] sm:leading-[100px] leading-[50px]">
               Iti multumim pentru interes!
             </h1>
-            <p className="text-white text-[40px] leading-[49.2px] font-[400] font-clashdisplay-regular">
+            <p className="text-white md:text-[40px] sm:text-[30px] text-[20px] leading-[49.2px] font-[400] font-clashdisplay-regular">
               Te vom contacta in curand.
             </p>
             <Link
               href="/contact"
-              className="font-clashdisplay-regular px-[40px] py-[20px] text-white text-[40px] border-2 border-[#FFFFFF1A] font-[400] leading-[49.2px] bg-[#FFFFFF52] hover:bg-[#ffffff62] rounded-[70px] transition duration-300 ease-in-out"
+              className="font-clashdisplay-regular md:px-[40px] sm:px-[30px] px-[20px] md:py-[20px] sm:py-[15px] py-[10px] text-white md:text-[40px] sm:text-[34px] text-[28px] border-2 border-[#FFFFFF1A] font-[400] leading-[49.2px] bg-[#FFFFFF52] hover:bg-[#ffffff62] rounded-[70px] transition duration-300 ease-in-out"
               style={{ zIndex: 10 }}
             >
               Go Home
