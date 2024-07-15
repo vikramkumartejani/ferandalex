f078203682a2026a8ffb41ac8e7f17548b3d7aa0
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-07-15 22:17:09 +0500

    About page animation added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,14 @@
+"use client";
 import React from "react";
 import Footer from "../components/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const page = () => {
+  React.useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <div className="text-white font-clashdisplay-regular">
       <section className="relative h-[400px] md:h-full w-full">
@@ -16,35 +23,50 @@ const page = () => {
           className="h-[400px] md:h-full w-full"
         />
         <img
-          src="/assets/DESPRE NOI.svg "
+          src="/assets/DESPRE NOI.svg"
           alt=""
-          className=" absolute top-[60%] lg:top-2/3 ml-[5%] w-[60%] lg:w-auto  "
+          className="absolute top-[60%] lg:top-2/3 ml-[5%] w-[60%] lg:w-auto"
+          data-aos="fade-up"
         />
-        {/* <h1 className="font-extrabold absolute top-[60%] lg:top-2/3 ml-[5%] uppercase text-[20px] leading-[25px] xl:text-[100px] x;:leading-[96px]">
-          DESPRE NOI: <br /> de la planuri la realitate
-        </h1> */}
       </section>
 
-      <section className="grid  md:grid-cols-2 lg:pt-[200px] items-center justify-center gap-[80px]  px-[5%] lg:px-0 lg:pl-[5%] max-h-[850px] py-[5%]">
+      <section
+        className="grid md:grid-cols-2 lg:pt-[200px] items-center justify-center gap-[80px] px-[5%] lg:px-0 lg:pl-[5%] max-h-[850px] py-[5%]"
+        data-aos="fade-right"
+      >
         <div>
-          <h3 className="text-[40px] lg:text-[100px] leading-[123px]">
+          <h3
+            className="text-[40px] lg:text-[100px] leading-[123px]"
+            data-aos="fade-up"
+          >
             Misiunea noastra
           </h3>
-          <p className="text-[20px] lg:text-[40px] leading-[30px] md:leading-[50px]">
+          <p
+            className="text-[20px] lg:text-[40px] leading-[30px] md:leading-[50px]"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
             Suntem o echipa de profesionisti pasionati, dedicati sa construim
             viitorul. Cu o experienta bogata, ne angajam sa depasim asteptarile
             si sa transformam visele in realitate.
           </p>
         </div>
 
-        <div className="hidden lg:block px-0 h-full w-full lg:rounded-l-[77px] overflow-hidden">
+        <div
+          className="hidden lg:block px-0 h-full w-full lg:rounded-l-[77px] overflow-hidden"
+          data-aos="fade-left"
+        >
           <img
             src="/assets/about-section-bg1.jpeg"
             className="h-full w-full object-contain"
           />
         </div>
 
-        <div className="lg:hidden w-[90%] h-full rounded-[50px] mx-auto overflow-hidden">
+        <div
+          className="lg:hidden w-[90%] h-full rounded-[50px] mx-auto overflow-hidden"
+          data-aos="fade-left"
+          data-aos-delay="200"
+        >
           <img
             src="/assets/about-section-bg1.jpeg"
             className="h-full w-full object-cover"
@@ -52,11 +74,21 @@ const page = () => {
         </div>
       </section>
 
-      <section className="space-y-[40px] lg:pt-[300px] md:space-y-[60px] lg:space-y-[150px] px-[10%]">
-        <h2 className="text-[40px] lg:text-[100px] leading-[123px] text-center">
+      <section
+        className="space-y-[40px] lg:pt-[300px] md:space-y-[60px] lg:space-y-[150px] px-[10%]"
+        data-aos="fade-up"
+      >
+        <h2
+          className="text-[40px] lg:text-[100px] leading-[123px] text-center"
+          data-aos="fade-up"
+        >
           Cine suntem
         </h2>
-        <p className="text-[20px] lg:text-[40px] leading-[30px] md:leading-[50px]">
+        <p
+          className="text-[20px] lg:text-[40px] leading-[30px] md:leading-[50px]"
+          data-aos="fade-up"
+          data-aos-delay="200"
+        >
           Ferandalex, o companie fondată în 2018 de un grup de profesioniști
           pasionați, a pornit cu o viziune clară: construirea unui viitor mai
           bun. <br /> <br /> De la început, Ferandalex s-a concentrat pe
@@ -72,7 +104,11 @@ const page = () => {
           diferență.
         </p>
 
-        <div className="relative w-full overflow-hidden">
+        <div
+          className="relative w-full overflow-hidden"
+          data-aos="fade-up"
+          data-aos-delay="400"
+        >
           <img
             src="/assets/about-section-bg2.jpeg"
             alt="Background"
@@ -84,12 +120,13 @@ const page = () => {
             alt=""
             className="absolute top-[60%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full"
           />
-          {/* <h1 className="text-[60px] lg:text-[220px] xl:text-[280px] text-start 3xl:text-[320px] leading-none lg:leading-[220px] font-extrabold absolute top-[60%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">
-            WE ARE A <span className="text-[#CCE716]">FAMILY</span>
-          </h1> */}
         </div>
 
-        <h2 className="text-[40px] lg:text-[100px] leading-[50px] lg:leading-[123px] text-center">
+        <h2
+          className="text-[40px] lg:text-[100px] leading-[50px] lg:leading-[123px] text-center"
+          data-aos="fade-up"
+          data-aos-delay="500"
+        >
           Angajamentul nostru
         </h2>
       </section>
@@ -104,7 +141,11 @@ const page = () => {
         </div>
 
         <div>
-          <p className="text-[20px] px-[10%] py-[15%] lg:text-[40px] leading-[30px] md:leading-[50px]">
+          <p
+            data-aos="fade-up"
+            data-aos-delay="800"
+            className="text-[20px] px-[10%] py-[15%] lg:text-[40px] leading-[30px] md:leading-[50px]"
+          >
             La Ferandalex, ne angajăm să oferim mai mult decât simple
             construcții. Garantăm excelență în fiecare aspect al muncii noastre,
             de la proiectare până la finalizare. <br /> <br /> Calitatea
