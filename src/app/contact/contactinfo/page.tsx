fce6abcd2375bb8fad8fe4fdc2c1a4a80c491a9f
Sharjeel <92874026+ShaYri12@users.noreply.github.com>
2024-07-15 06:17:48 +0500

    Update page.tsx

--- src/app/contact/contactinfo/page.tsx
+++ src/app/contact/contactinfo/page.tsx
@@ -21,92 +21,92 @@ const Contact = () => {
         <img
           height={"100%"}
           width={"100%"}
-          className="object-cover h-full w-full p-[30px]"
+          className="object-cover h-full w-full lg:p-[30px] md:p-[20px] p-[10px]"
           src="/assets/contact-img2.svg"
           alt="contact-img"
         />
       </div>
-      <div className="flex lg:flex-row flex-col md:items-center relative lg:ps-[50px] md:ps-0 ps-[50px] z-[2] pt-[200px] pb-[178px]">
+      <div className="flex lg:flex-row flex-col md:items-center relative lg:ps-[50px] md:ps-0 sm:ps-[50px] ps-[25px] z-[2] pt-[200px] pb-[178px]">
         <form
           onSubmit={handleSubmit}
-          className="w-2/3 flex flex-col justify-center text-white"
+          className="lg:w-2/3 sm:w-[90%] w-[95%] flex flex-col justify-center text-white"
         >
-          <div className="flex flex-col md:gap-[91px] gap-[20px]">
-            <div className="flex md:flex-row flex-col lg:gap-[130px] gap-[20px]">
-              <div className="flex flex-col flex-1 gap-[26px]">
+          <div className="flex flex-col md:gap-[91px] gap-[50px]">
+            <div className="flex md:flex-row flex-col lg:gap-[130px] gap-[50px]">
+              <div className="flex flex-col flex-1 md:gap-[26px] gap-[10px]">
                 <label
                   htmlFor="name"
-                  className="block text-[40px] font-[400] leading-[49.2px]"
+                  className="block md:text-[40px] text-[30px] font-[400] md:leading-[49.2px] leading-[30px]"
                 >
                   Nume
                 </label>
                 <input
                   type="text"
                   id="name"
-                  className="shadow backdrop-blur-sm appearance-none h-[73px] border-2 border-[#FFFFFF1A] rounded-[70px] bg-[#FFFFFF4D] w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow backdrop-blur-sm appearance-none sm:h-[73px] h-[60px] border-2 border-[#FFFFFF1A] rounded-[70px] bg-[#FFFFFF4D] w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
               </div>
-              <div className="flex flex-1 flex-col gap-[26px]">
+              <div className="flex flex-col flex-1 md:gap-[26px] gap-[10px]">
                 <label
                   htmlFor="phone"
-                  className="block text-[40px] font-[400] leading-[49.2px]"
+                  className="block md:text-[40px] text-[30px] font-[400] md:leading-[49.2px] leading-[30px]"
                 >
                   Telefon
                 </label>
                 <input
                   type="tel"
                   id="phone"
-                  className="shadow appearance-none h-[73px] border-2 border-[#FFFFFF1A] rounded-[70px] bg-[#FFFFFF4D] w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none sm:h-[73px] h-[60px] border-2 border-[#FFFFFF1A] rounded-[70px] bg-[#FFFFFF4D] w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
                   value={phone}
                   onChange={(e) => setPhone(e.target.value)}
                 />
               </div>
             </div>
-            <div className="flex md:flex-row flex-col lg:gap-[130px] gap-[20px]">
-              <div className="flex flex-1 flex-col gap-[26px]">
+            <div className="flex md:flex-row flex-col lg:gap-[130px] gap-[50px]">
+              <div className="flex flex-col flex-1 md:gap-[26px] gap-[10px]">
                 <label
                   htmlFor="company"
-                  className="block text-[40px] font-[400] leading-[49.2px]"
+                  className="block md:text-[40px] text-[30px] font-[400] md:leading-[49.2px] leading-[30px]"
                 >
                   Companie
                 </label>
                 <input
                   type="text"
                   id="company"
-                  className="shadow backdrop-blur-sm appearance-none h-[73px] border-2 border-[#FFFFFF1A] rounded-[70px] bg-[#FFFFFF4D] w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow backdrop-blur-sm appearance-none sm:h-[73px] h-[60px] border-2 border-[#FFFFFF1A] rounded-[70px] bg-[#FFFFFF4D] w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
                   value={company}
                   onChange={(e) => setCompany(e.target.value)}
                 />
               </div>
-              <div className="flex flex-1 flex-col gap-[26px]">
+              <div className="flex flex-col flex-1 md:gap-[26px] gap-[10px]">
                 <label
                   htmlFor="jobFunction"
-                  className="block text-[40px] font-[400] leading-[49.2px]"
+                  className="block md:text-[40px] text-[30px] font-[400] md:leading-[49.2px] leading-[30px]"
                 >
                   Funcție
                 </label>
                 <input
                   type="text"
                   id="jobFunction"
-                  className="shadow backdrop-blur-sm appearance-none h-[73px] border-2 border-[#FFFFFF1A] rounded-[70px] bg-[#FFFFFF4D] w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow backdrop-blur-sm appearance-none sm:h-[73px] h-[60px] border-2 border-[#FFFFFF1A] rounded-[70px] bg-[#FFFFFF4D] w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
                   value={jobFunction}
                   onChange={(e) => setJobFunction(e.target.value)}
                 />
               </div>
             </div>
             <div className="flex flex-grow">
-              <div className="flex flex-col gap-[26px] flex-grow">
+              <div className="flex flex-col  md:gap-[26px] gap-[10px] flex-grow">
                 <label
                   htmlFor="description"
-                  className="block text-[40px] font-[400] leading-[49.2px]"
+                  className="block md:text-[40px] text-[30px] font-[400] md:leading-[49.2px] leading-[30px]"
                 >
                   Descriere proiect
                 </label>
                 <textarea
                   id="description"
-                  className="shadow backdrop-blur-sm appearance-none border-2 border-[#FFFFFF1A] rounded-[70px] bg-[#FFFFFF4D] h-[251px] w-full py-[20px] px-[30px] leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow backdrop-blur-sm appearance-none border-2 border-[#FFFFFF1A] sm:rounded-[70px] rounded-[20px] bg-[#FFFFFF4D] sm:h-[251px] h-[230px] w-full py-[20px] px-[30px] leading-tight focus:outline-none focus:shadow-outline"
                   value={description}
                   onChange={(e) => setDescription(e.target.value)}
                 ></textarea>
@@ -117,7 +117,7 @@ const Contact = () => {
         <div className="flex items-center justify-center mx-auto lg:py-0 py-[40px]">
           <Link
             href="/contact/thankyou"
-            className="text-[70px] font-[400] leading-[69.02px] text-white backdrop-blur-sm"
+            className="md:text-[70px] text-[50px] font-[400] leading-[69.02px] text-white backdrop-blur-sm"
           >
             Trimite
           </Link>
