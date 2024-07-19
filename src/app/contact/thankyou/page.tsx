adafe254776cd4cf31dc8a800f880d77ab18ca6f
ShaYri12 <sharjeelb60@gmail.com>
2024-07-19 05:26:54 +0500

    fix the contact thank you

--- src/app/contact/thankyou/page.tsx
+++ src/app/contact/thankyou/page.tsx
@@ -23,32 +23,32 @@ const ThankYou = () => {
             data-aos-delay="200" // Delay before the animation starts
             height={"100%"}
             width={"100%"}
-            className="object-cover md:p-[30px] sm:p-[20px] p-[10px] h-[1172px] w-full"
+            className="object-cover md:p-[30px] sm:p-[20px] p-[10px] h-[125%] w-full"
             src="/assets/contact-img2.svg"
             alt="contact-img"
           />
           <div
             data-aos="fade-up" // AOS animation
             data-aos-delay="500" // Delay before the animation starts
-            className="w-full absolute top-1/2 flex flex-col items-center justify-center sm:gap-[30px] gap-[18px] px-[30px]"
+            className="w-full h-[125%] absolute top-0 flex flex-col items-center justify-center sm:gap-[30px] gap-[18px] px-[30px]"
           >
             <h1
               data-aos="zoom-in" // AOS animation
               data-aos-delay="700" // Delay before the animation starts
-              className="max-w-[800px] text-center justify-center text-white md:text-[121px] sm:text-[89px] text-[40px] font-[400] md:leading-[119.31px] sm:leading-[100px] leading-[50px]"
+              className="max-w-[800px] text-center justify-center text-white lg:text-[121px] md:text-[105px] sm:text-[89px] text-[40px] font-[400] lg:leading-[119.31px] md:leading-[110.31px] sm:leading-[100px] leading-[50px]"
             >
               Iti multumim pentru interes!
             </h1>
             <p
               data-aos="zoom-in" // AOS animation
               data-aos-delay="900" // Delay before the animation starts
-              className="text-white md:text-[40px] sm:text-[30px] text-[20px] leading-[49.2px] font-[400] font-clashdisplay-regular"
+              className="text-white lg:text-[40px] md:text-[35px] sm:text-[30px] text-[20px] lg:leading-[49.2px] md:leading-[35px] leading-[25px] font-[400] font-clashdisplay-regular"
             >
               Te vom contacta in curand.
             </p>
             <Link
               href="/contact"
-              className="font-clashdisplay-regular md:px-[40px] sm:px-[30px] px-[20px] md:py-[20px] sm:py-[15px] py-[10px] text-white md:text-[40px] sm:text-[34px] text-[28px] border-2 border-[#FFFFFF1A] font-[400] leading-[49.2px] bg-[#FFFFFF52] hover:bg-[#ffffff62] rounded-[70px] transition duration-300 ease-in-out"
+              className="font-clashdisplay-regular lg:px-[40px] sm:px-[30px] px-[20px] lg:py-[20px] md:py-[15px] sm:py-[15px] py-[10px] text-white md:text-[40px] sm:text-[34px] text-[28px] border-2 border-[#FFFFFF1A] font-[400] leading-[49.2px] bg-[#FFFFFF52] hover:bg-[#ffffff62] rounded-[70px] transition duration-300 ease-in-out"
             >
               Go Home
             </Link>
